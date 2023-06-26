6ecb853dff445d439d1471f23a0c07d6ebf5b477
Shalki <swenushika@gmail.com>
2023-06-26 16:08:16 +0530

    Update server

--- src/index.ts
+++ src/index.ts
@@ -44,8 +44,8 @@ app.get('/users', (req, res) => {
 });
 
 
-const PORT = 3000;
+const port = 3000;
 
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App listening on port ${port}`);
 });
