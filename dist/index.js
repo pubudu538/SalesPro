ed062c2ef6a875d69ae02caf7d3a3755a60d7a1b
Pubudu Gunatilaka <pubudu538@gmail.com>
2023-07-03 19:50:22 +0530

    Adding a dockerfile for Choreo

--- dist/index.js
+++ dist/index.js
@@ -44,7 +44,7 @@ app.get('/users', (req, res) => {
         res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
     });
 });
-const PORT = process.env.PORT || '3000';
+const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`App listening on port ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ app.get('/users', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || '3000';
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen( PORT, '0.0.0.0', () => {
   console.log(`App listening on port ${PORT}`);
